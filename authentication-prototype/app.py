80324fe3aca2941244ededac982a20dbbbb8db4f
Greg Lahaye <gregory.lahaye@student.curtin.edu.au>
2021-04-20 19:15:48 +0800

    add authentication prototype

---
+++ authentication-prototype/app.py
@@ -0,0 +1,80 @@
+import os
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import current_user
+from flask_jwt_extended import jwt_required
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data['sub']
+    return User.query.filter_by(id=identity).one_or_none()
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    user = User(
+        email=email,
+        password=generate_password_hash(password),
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    user = User.query.filter_by(email=email).one_or_none()
+    if not user or not user.check_password(password):
+        return jsonify('Wrong username or password'), 401
+
+    access_token = create_access_token(identity=user)
+    return jsonify(access_token=access_token)
+
+
+@app.route('/me', methods=['GET'])
+@jwt_required()
+def protected():
+    return jsonify(
+        id=current_user.id,
+        email=current_user.email,
+    )
+
+
+if __name__ == '__main__':
+    app.run()
