d7ad1c18feab7ab4f2f6838c7de079c3ad90b4cd
Ryan Martin <19758152@student.curtin.edu.au>
2021-04-30 11:41:39 +0800

    Validation functions return astropy.unit value

--- search/validate_input.py
+++ search/validate_input.py
@@ -1,5 +1,7 @@
 import re 
 
+from astropy import units as u 
+
 # Validates RA/DEC/Radius in sexagesimal or floating point value. 
 # Demonstrates regex patterns, radius must be 
 #
@@ -10,27 +12,38 @@ import re
 RA_REGEX = re.compile('[0-9]+(\.[0-9]+)?h[0-9]+(\.[0-9]+)?m[0-9]+(\.[0-9]+)?s')
 # [float]d[float]m[float]s
 DEC_REGEX = re.compile('[0-9]+(\.[0-9]+)?d[0-9]+(\.[0-9]+)?m[0-9]+(\.[0-9]+)?s')
-
+#
+# For all of these validation functions, they will return u.deg (decimal degrees)
+# defined in the astroquery.units module (you may see a Module has no member error in 
+# your IDE, however it seems to be a bug). If not float, return None for 
+# sexagesimal notation. 
+#
+# The reason we need to return the units in these methods is because the units
+# must be defined in a vector for the Simbad query methods that correlates to 
+# each value. 
+#
+# The units are invalid if the function returns False. 
+#
 def validRA(ra_value):
     if (isFloat(ra_value)):
-        return True 
+        return u.deg
     else:
         if re.match(RA_REGEX, ra_value):
-            return True
+            return None
     return False 
 
 def validDEC(dec_value):
     if (isFloat(dec_value)):
-        return True 
+        return u.deg
     else:
         if re.match(DEC_REGEX, dec_value):
-            return True
+            return None
     return False 
 
 def validRadius(rad_value):
     if isFloat(rad_value):
         if float(rad_value) <= 0.5 and float(rad_value) >= 0.0:
-            return True 
+            return u.deg
     return False
 
 def isFloat(value):
@@ -45,18 +58,9 @@ def main():
     dec = input('DEC: ')
     rad = input('Radius: ')
 
-    if not validRA(ra): 
-        print('Invalid RA value!')
-    else:
-        print('RA = valid')
-    if not validDEC(dec):
-        print('Invalid DEC value!')
-    else:
-        print('DEC = valid')
-    if not validRadius(rad):
-        print('Invalid radius value!')
-    else:
-        print('Radius = valid')
+    print('RA:', validRA(ra))
+    print('DEC:', validDEC(dec))
+    print('Radius:', validRadius(rad))
 
 if __name__ == '__main__':
     main()
