a6e4a6e9948d1d60a7f585d966ef3e6a507945b3
Ryan Martin <Ryan.K.Martin@student.curtin.edu.au>
2021-08-01 20:40:24 +0800

    Added type aliasing

--- backend/controller/search/vis.py
+++ backend/controller/search/vis.py
@@ -2,8 +2,13 @@ from typing import List, Tuple
 from datetime import datetime 
 
 
+# Type aliasing for the list data structures. 
+ReportsList = List[Tuple[int, datetime]]
+EdgesList = List[Tuple[int, int]]
+
+
 def create_nodes_list(reports_list: List[ReportResult]
-) -> Tuple[List[Tuple[int, datetime]], List[Tuple[int, int]]]:
+) -> Tuple[ReportsList, EdgesList]:
     ''' Constructs the data structures required to graph relations between
         ATel reports. The data is structured like a standard graph, involving
         a list of nodes and edges. Reports are considered related by their 
@@ -15,10 +20,10 @@ def create_nodes_list(reports_list: List[ReportResult]
         returned from a successful search. 
 
     Returns:
-        List[Tuple[int, datetime]]: the nodes list for the graph. The list
+        ReportsList: the nodes list for the graph. The list
             contains a tuple representing an integer for the ATel number and a 
             datetime field for the report's observation date. 
-        List[Tuple[int, int]]: the edge list. An edge is represented by a pair
+        EdgesList: the edge list. An edge is represented by a pair
             of ATel numbers. 
     '''
     return [], [] # Stub
