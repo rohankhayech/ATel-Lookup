dc79c7f40683f8881e4de0003ec56187e71e4c92
Nathan Sutardi <nathan.sutardi@student.curtin.edu.au>
2021-08-08 01:23:52 +0800

    Added a test for HTML downloader

---
+++ backend/controller/importer/test_importer.py
@@ -0,0 +1,42 @@
+"""
+Testing for importer functions.
+
+Author:
+    Nathan Sutardi
+
+License Terms and Copyright:
+    Copyright (C) 2021 Nathan Sutardi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import unittest
+import importer
+
+from bs4 import BeautifulSoup
+
+class TestImporterFunctions(unittest.TestCase):
+    # Tests download_report function
+    def test_html_download(self):
+        # ATel report titles for comparison
+        titles = ['QPOs in 4U 1626-67', 'GB971227', 'Improved Coordinates for GB971227']
+
+        # Tests HTML downloader for 3 ATels by comparing title outputs
+        for i in range(3):
+            html = importer.download_report(i + 1)
+            soup = BeautifulSoup(html, 'html.parser')
+            self.assertEqual(soup.find('h1', {'class': 'title'}).get_text(), titles[i])
+
+if __name__ == "__main__":
+    unittest.main()
