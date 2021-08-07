b21e3bc13ac775eccdc5733fc3e5eceb69315918
Nathan Sutardi <nathan.sutardi@student.curtin.edu.au>
2021-08-07 22:46:27 +0800

    Added header for importer.py

--- backend/controller/importer/importer.py
+++ backend/controller/importer/importer.py
@@ -1,3 +1,27 @@
+"""
+Contains functions that handles the import of ATel reports as well as the
+parsing and extraction of ATel data.
+
+Author:
+    Nathan Sutardi
+
+License Terms and Copyright:
+    Copyright (C) 2021 Nathan Sutardi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from datetime import datetime
 from typing import List
 from astropy.coordinates import SkyCoord
@@ -37,6 +61,8 @@ def download_report(atel_num: int) -> str:
         NetworkErrorException: Thrown when network failure occurs during the HTML download.
         DownloadFailException: Thrown when the HTML could not be downloaded.
     """
+
+
     return ''
 
 def parse_report(html_string: str) -> ImportedReport:
