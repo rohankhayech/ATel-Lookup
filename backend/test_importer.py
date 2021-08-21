9b3aa38347b00784279631726119a6ab9b2c27db
Nathan Sutardi <nathan.sutardi@student.curtin.edu.au>
2021-08-22 00:54:58 +0800

    Created unit test for HTML parser

--- backend/test_importer.py
+++ backend/test_importer.py
@@ -24,6 +24,8 @@ License Terms and Copyright:
 import unittest
 
 from controller.importer.importer import *
+from model.ds.report_types import ImportedReport
+
 from bs4 import BeautifulSoup
 from unittest import mock
 from requests.exceptions import ConnectionError, HTTPError
@@ -32,7 +34,7 @@ from pyppeteer.errors import TimeoutError
 # Importer functions
 class TestImporterFunctions(unittest.TestCase):
     # Tests download_report function
-    def test_html_download(self):
+    '''def test_html_download(self):
         # ATel report titles for comparison
         titles = ['QPOs in 4U 1626-67', 'GB971227', 'Improved Coordinates for GB971227']
 
@@ -40,7 +42,64 @@ class TestImporterFunctions(unittest.TestCase):
         for i in range(3):
             html = download_report(i + 1)
             soup = BeautifulSoup(html, 'html.parser')
-            self.assertEqual(soup.find('h1', {'class': 'title'}).get_text(), titles[i])
+            self.assertEqual(soup.find('h1', {'class': 'title'}).get_text(), titles[i])'''
+
+    # Tests parse_report function
+    @mock.patch('controller.importer.importer.extract_keywords')
+    def test_html_parser(self, mock_get):
+        mock_get.return_value = []
+
+        # Parses HTML of ATel #1000
+        f = open('atel1000.html', 'r')
+        imported_report = parse_report(1000, f.read())
+        f.close()
+
+        # Retrieves ATel #1000 body text
+        f = open('atel1000_body.txt', 'r')
+        body = f.read()
+        f.close()
+
+        self.assertEqual(imported_report.atel_num, 1000)
+        self.assertEqual(imported_report.title, 'INTEGRAL observations of GX339-4: preliminary spectral fit results')
+        self.assertEqual(imported_report.authors, 'M. D. Caballero-Garcia (LAEFF/INTA), J. Miller (Univ. of Michigan), E. Kuulkers (ESA/ESAC), M. Diaz Trigo (ESA/ESAC), on behalf of a larger collaboration')
+        self.assertEqual(imported_report.body, body)
+        self.assertEqual(imported_report.submission_date.day, 11)
+        self.assertEqual(imported_report.submission_date.month, 2)
+        self.assertEqual(imported_report.submission_date.year, 2007)
+        self.assertEqual(imported_report.submission_date.hour, 9)
+        self.assertEqual(imported_report.submission_date.minute, 48)
+        self.assertCountEqual(imported_report.referenced_reports, [])
+        self.assertCountEqual(imported_report.observation_dates, [])
+        self.assertCountEqual(imported_report.keywords, [])
+        self.assertCountEqual(imported_report.objects, [])
+        self.assertCountEqual(imported_report.coordinates, [])
+        self.assertCountEqual(imported_report.referenced_by, [])
+
+        # Parses HTML of ATel #10000
+        f = open('atel10000.html', 'r')
+        imported_report = parse_report(10000, f.read())
+        f.close()
+
+        # Retrieves ATel #10000 body text
+        f = open('atel10000_body.txt', 'r')
+        body = f.read()
+        f.close()
+
+        self.assertEqual(imported_report.atel_num, 10000)
+        self.assertEqual(imported_report.title, 'ASASSN-17bd: Discovery of A Probable Supernova in 2MASX J15591858+1336487')
+        self.assertEqual(imported_report.authors, 'J. Brimacombe (Coral Towers Observatory), J. S. Brown, K. Z. Stanek, T. W.-S. Holoien, C. S. Kochanek, J. Shields, T. A. Thompson (Ohio State), B. J. Shappee (Hubble Fellow, Carnegie Observatories), J. L. Prieto (Diego Portales; MAS), D. Bersier (LJMU), Subo Dong, S. Bose, Ping Chen (KIAA-PKU), R. A. Koff (Antelope Hills Observatory), G. Masi (Virtual Telescope Project, Ceccano, Italy), R. S. Post (Post Astronomy), G. Stone (Sierra Remote Observatories)')
+        self.assertEqual(imported_report.body, body)
+        self.assertEqual(imported_report.submission_date.day, 25)
+        self.assertEqual(imported_report.submission_date.month, 1)
+        self.assertEqual(imported_report.submission_date.year, 2017)
+        self.assertEqual(imported_report.submission_date.hour, 5)
+        self.assertEqual(imported_report.submission_date.minute, 0)
+        self.assertCountEqual(imported_report.referenced_reports, [])
+        self.assertCountEqual(imported_report.observation_dates, [])
+        self.assertCountEqual(imported_report.keywords, [])
+        self.assertCountEqual(imported_report.objects, [])
+        self.assertCountEqual(imported_report.coordinates, [])
+        self.assertCountEqual(imported_report.referenced_by, [])
 
     # Tests extract_keywords function
     def test_keywords_extractor(self):
