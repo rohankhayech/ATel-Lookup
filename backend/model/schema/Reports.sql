c79d95a0bde7cda2b82259c2600d8870297f04c5
Rohan Khayech <rohan.khayech@student.curtin.edu.au>
2021-08-23 19:21:02 +0800

    Increased char limits for Reports table.

--- backend/model/schema/Reports.sql
+++ backend/model/schema/Reports.sql
@@ -1,7 +1,7 @@
 create table if not exists Reports (
     atelNum int unsigned primary key,
-    title varchar(255) not null,
-    authors varchar(255) not null,
+    title varchar(1024) not null,
+    authors varchar(1024) not null,
     body varchar(4000) not null,
     submissionDate timestamp not null,
     keywords set('{}') not null default ''
