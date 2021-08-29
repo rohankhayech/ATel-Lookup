3eb1c8d149b4d5168a1c72d18c1921247fec9bd0
Ryan Martin <Ryan.K.Martin@student.curtin.edu.au>
2021-08-29 14:42:22 +0800

    Added unit testing for check_object_updates

--- backend/test/test_main_search.py
+++ backend/test/test_main_search.py
@@ -22,3 +22,66 @@ License Terms and Copyright:
     You should have received a copy of the GNU Affero General Public License
     along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
+
+
+from controller.search.query_simbad import QuerySimbadError
+from datetime import datetime, timedelta
+import unittest as ut 
+from unittest.mock import MagicMock
+
+from controller.search import query_simbad as qs
+from controller.search import search
+
+
+def none_return():
+    return None 
+
+
+class TestCheckObjectUpdates(ut.TestCase):
+    def setUp(self):
+        self.dt_now = datetime.now()
+        self.dt_almost = datetime.now() - timedelta(days=59)
+        self.dt_exact = datetime.now() - timedelta(days=60)
+        self.dt_old = datetime.now() - timedelta(days=200)
+
+
+    def test_up_to_date(self):
+        search_mock = search
+        search_mock.qs.get_aliases = MagicMock()
+
+        search_mock._check_object_updates("object", self.dt_now) 
+        search_mock.qs.get_aliases.assert_not_called()
+        search_mock._check_object_updates("object", self.dt_almost) 
+        search_mock.qs.get_aliases.assert_not_called()
+
+
+    def test_needs_update_no_network(self):
+        search_mock = search 
+
+        search_mock.qs.get_aliases = MagicMock(side_effect=QuerySimbadError("Error")) 
+
+        with self.assertRaises(QuerySimbadError):
+            search_mock._check_object_updates("object", self.dt_old)
+            search_mock._check_object_updates("object", self.dt_exact)
+
+
+    def test_needs_update(self):
+        search_mock = search 
+        
+        search_mock.db.add_aliases = MagicMock() 
+
+        # Case 1: SIMBAD does not find any aliases for the object. 
+        search_mock.qs.get_aliases = MagicMock(return_value=[])
+        search_mock._check_object_updates("test", self.dt_old)
+        search_mock.db.add_aliases.assert_called_once_with("test", [])
+
+        search_mock.db.add_aliases = MagicMock() 
+
+        # Case 2: SIMBAD query returns alias list 
+        search_mock.qs.get_aliases = MagicMock(return_value=["alias1", "alias2", "alias3"])
+        search_mock._check_object_updates("test", self.dt_old)
+        search_mock.db.add_aliases.assert_called_once_with("test", ["alias1", "alias2", "alias3"])
+
+
+if __name__ == '__main__':
+    ut.main()
