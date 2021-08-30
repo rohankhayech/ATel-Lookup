39184a113f64914902b30f6d0dfda8381a20f7f7
Rohan Khayech <rohan.khayech@student.curtin.edu.au>
2021-08-30 14:40:48 +0800

    Added ObjectRefs table schema.

---
+++ backend/model/schema/ObjectRefs.sql
@@ -0,0 +1,7 @@
+create table ObjectRefs (
+    atelNumFK int not null,
+    objectIDFK varchar (255) not null,
+    foreign key (atelNumFK) references Reports(atelNum) on update cascade on delete cascade,
+    foreign key (objectIDFK) references Objects(objectID) on update cascade on delete cascade,
+    primary key (atelNumFK, objectIDFK)
+)
