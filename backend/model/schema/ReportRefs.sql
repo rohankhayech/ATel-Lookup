ba32e51e08c94fe68fcc9a04069f694ca48d7640
Rohan Khayech <rohan.khayech@student.curtin.edu.au>
2021-09-20 00:08:45 +0800

    Removed foreign key constraint to allow adding references from reports not yet imported.

--- backend/model/schema/ReportRefs.sql
+++ backend/model/schema/ReportRefs.sql
@@ -1,7 +1,6 @@
 create table if not exists ReportRefs (
-    atelNumFK int unsigned not null,
+    atelNum int unsigned not null,
     refReportFK int unsigned not null,
-    foreign key (atelNumFK) references Reports(atelNum) on update cascade on delete cascade,
     foreign key (refReportFK) references Reports(atelNum) on update cascade on delete cascade,
     primary key (atelNumFK, refReportFK)
 )
