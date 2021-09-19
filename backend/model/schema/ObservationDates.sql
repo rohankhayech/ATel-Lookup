a90382e5b12d173a7b3206741270b37eff8a5830
Rohan Khayech <rohan.khayech@student.curtin.edu.au>
2021-09-19 22:30:12 +0800

    Fixed ObservationDates table schema.

--- backend/model/schema/ObservationDates.sql
+++ backend/model/schema/ObservationDates.sql
@@ -2,5 +2,5 @@ create table if not exists ObservationDates (
     atelNumFK int unsigned not null,
     obDate timestamp not null,
     foreign key (atelNumFK) references Reports(atelNum) on update cascade on delete cascade,
-    primary key (atelNumFK, obDate),
+    primary key (atelNumFK, obDate)
 )
