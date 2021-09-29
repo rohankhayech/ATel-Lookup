3c5e806854b2ba03a255b13873b92f0c63a89281
Greg Lahaye <greg@lahayes.net>
2021-09-29 11:00:28 +0800

    Fix metadata

--- frontend/src/app/admin-page/admin-page.component.ts
+++ frontend/src/app/admin-page/admin-page.component.ts
@@ -1,24 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { ImportService } from '../import.service';
+import { Metadata } from '../metadata.interface';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
   public id = 0;
+
+  public upcoming?: number;
 
   public loadingAuto = false;
   public loadingManual = false;
 
   constructor(
+    private http: HttpClient,
     private importService: ImportService,
     private snackBar: MatSnackBar
   ) {}
+
+  ngOnInit() {
+    this.fetchMetadata();
+  }
 
   importAll() {
     return this.importService.importAll().pipe(
@@ -52,5 +62,13 @@ export class AdminPageComponent {
         return throwError(error);
       })
     );
+  }
+
+  fetchMetadata() {
+    this.http
+    .get<Metadata>(`${environment.apiUrl}/metadata`)
+    .subscribe((metadata) => {
+      this.upcoming = metadata.nextAtel;
+    });
   }
 }
